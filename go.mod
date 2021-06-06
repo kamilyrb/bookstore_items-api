ef80eb6d75879f86d12eb29122b84cff0b48dca0
Kamil Yarbaşı <kamilyrb@gmail.com>
2021-06-07 00:53:57 +0300

    feature/oauth-version - upgrade oauth version - upgrade eroros version

--- go.mod
+++ go.mod
@@ -6,10 +6,9 @@ require (
 	github.com/fortytw2/leaktest v1.3.0 // indirect
 	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/gorilla/mux v1.8.0
-	github.com/kamilyrb/bookstore_oauth-go v0.0.0-20210606000521-5a4425a6efe9
-	github.com/kamilyrb/bookstore_utils-go v0.0.0-20210606193045-f623220045ca
+	github.com/kamilyrb/bookstore_oauth-go v0.0.0-20210606212954-1ea5cf590762
+	github.com/kamilyrb/bookstore_utils-go v0.0.0-20210606210809-6c85c9ba4d5f
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/olivere/elastic v6.2.35+incompatible
 	github.com/pkg/errors v0.9.1 // indirect
-	go.uber.org/zap v1.17.0
 )
--- services/item_service.go
+++ services/item_service.go
@@ -10,20 +10,20 @@ var (
 )
 
 type itemsServiceInterface interface {
-	Create(items.Item) (*items.Item, *rest_errors.RestErr)
-	Get(string) (*items.Item, *rest_errors.RestErr)
+	Create(items.Item) (*items.Item, rest_errors.RestErr)
+	Get(string) (*items.Item, rest_errors.RestErr)
 }
 
 type itemsService struct {
 }
 
-func (s *itemsService) Create(item items.Item) (*items.Item, *rest_errors.RestErr) {
+func (s *itemsService) Create(item items.Item) (*items.Item, rest_errors.RestErr) {
 	if err := item.Save(); err != nil {
 		return nil, err
 	}
 	return &item, nil
 }
 
-func (s *itemsService) Get(id string) (*items.Item, *rest_errors.RestErr) {
+func (s *itemsService) Get(id string) (*items.Item, rest_errors.RestErr) {
 	return nil, rest_errors.NewInternalServerError("not implemented", nil)
 }
